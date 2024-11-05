ede14a0b9b8d86a9c7af60be2070557f4b64d278
Vinizin002 <167202566+Vinizin002@users.noreply.github.com>
2024-11-05 10:29:08 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Football',
+    'Em que data o time Corinthians foi fundado?',
+    '1 de Setembro de 1910'
+)
+
+criaCartao(
+    'Football',
+    'Quantos mundiais o time Corinthians possui?',
+    '2 mundiais'
+)
+
+criaCartao(
+    'Football',
+    'Quem ganhou a Champions League de 2004?',
+    'Porto FC'
+)
+
+criaCartao(
+    'materia',
+    'Qual foi o técnico do time vencedor da Champions League de 1977?',
+    'Brian Clough'
+)
